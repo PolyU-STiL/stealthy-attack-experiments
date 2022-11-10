4e3fdc6b62d6425ab98e83b257cbbe131563bd85
kushr-zz <77181117+kushr-zz@users.noreply.github.com>
2022-11-10 17:26:43 +0800

    update to 5 metrics

--- src/metrics.py
+++ src/metrics.py
@@ -4,48 +4,57 @@ import argparse
 import os
 import sys
 
-parser = argparse.ArgumentParser(description=globals()['__doc__'])
-# diffusion models
-parser.add_argument('--dataset', type=str, default='Cora', help='Cora or CiteSeer')
-parser.add_argument('--attack', type=str, default='CLGA', help='attack algorithm')
-parser.add_argument('--budget', type=str, default='1', help='attack budget(%)')
-parser.add_argument('--metric', type=str, default='node_homophily', help='metric algorithm')
-args = parser.parse_args()
 
-matrix_dir_path = '../poisoned_graph/{}/{}/{}%'.format(args.attack, args.dataset, args.budget)  # ./CLGA/Cora/1%
+def arg_parse():
+    parser = argparse.ArgumentParser(description=globals()['__doc__'])
+    parser.add_argument('--exp', type=str, default='1', help='experiment folder')
+    parser.add_argument('--dataset', type=str, default='Cora', help='Cora or CiteSeer')
+    parser.add_argument('--attack', type=str, default='CLGA', help='attack algorithm')
+    parser.add_argument('--budget', type=str, default='01', help='attack budget(%)')
+    parser.add_argument('--metric', type=str, default='balanced_homophily', help='metric algorithm')
+    args = parser.parse_args()
+    return args
 
-for root, dirs, files in os.walk(matrix_dir_path):
-    file_name = files[0]
 
-ally_path = './dataset/Cora/Citation/Cora/raw/ind.cora.ally'
-ty_path = './dataset/Cora/Citation/Cora/raw/ind.cora.ty'
-idx_path = './dataset/Cora/Citation/Cora/raw/ind.cora.test.index'
+def get_matrix(args):
 
-f = open(os.path.join(matrix_dir_path, file_name), 'rb')
-matrix = np.array(pickle.load(f), dtype=int)
+    matrix_dir_path = '../poisoned_graph/experiment{}/{}_{}_0.{}0000_adj.pkl'.format(args.exp,  args.dataset, args.attack, args.budget)  # ../poisoned_graph/experiment1/Cora_CLGA_0.010000_adj.pkl
+    for root, dirs, files in os.walk(matrix_dir_path):
+        file_name = files[0]
+    f = open(os.path.join(matrix_dir_path, file_name), 'rb')
+    matrix = np.array(pickle.load(f), dtype=int)
+    return matrix
 
-fy = open(ally_path, 'rb')
-ally = np.array(pickle.load(fy))
 
-fty = open(ty_path, 'rb')
-ty = np.array(pickle.load(fty))
+def get_label(args):
+    ally_path = './dataset/Cora/Citation/Cora/raw/ind.cora.ally'
+    ty_path = './dataset/Cora/Citation/Cora/raw/ind.cora.ty'
+    idx_path = './dataset/Cora/Citation/Cora/raw/ind.cora.test.index'
+    if args.dataset == 'Cora':
+        node_n = 2708
+    fy = open(ally_path, 'rb')
+    ally = np.array(pickle.load(fy))
 
-idx = np.loadtxt(idx_path)
-idx = np.array(idx, dtype=np.int)
-# print(idx.max())
+    fty = open(ty_path, 'rb')
+    ty = np.array(pickle.load(fty))
 
-label = np.zeros([2708]) - 1
-# initialize label
-for i in range(matrix.shape[0]):
-    if i < 1708:
-        label[i] = ally[i].argmax()
-    else:
-        for j in range(len(idx)):
-            if idx[j] == i:
-                label[i] = ty[j].argmax()
+    idx = np.loadtxt(idx_path)
+    idx = np.array(idx, dtype=np.int)
+    # print(idx.max())
 
-# metric evaluate
-if args.metric == 'edge_homophily':
+    label = np.zeros([node_n]) - 1
+    # initialize label
+    for i in range(node_n):
+        if i < 1708:
+            label[i] = ally[i].argmax()
+        else:
+            for j in range(len(idx)):
+                if idx[j] == i:
+                    label[i] = ty[j].argmax()
+    return np.array(label, dtype=int)
+
+
+def edge_homophily(matrix, label):
     count = 0
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
@@ -56,9 +65,10 @@ if args.metric == 'edge_homophily':
                     count += 1
     medges = matrix.sum() / 2
     eh = count / medges
-    print("dataset={}, attack={}, budget={}%,metric={}, result= {}".format(args.dataset, args.attack, args.budget,
-                                                                           args.metric, eh))
-if args.metric == 'node_homophily':
+    return eh
+
+
+def node_homophily(matrix, label):
     n = matrix.shape[0]
     count = 0.0
     for i in range(matrix.shape[0]):
@@ -74,5 +84,82 @@ if args.metric == 'node_homophily':
         count += nh_per_node
 
     nh = count / n
+    return nh
+
+
+def class_homophily(matrix, label):
+    C = label.max()
+    c_h = 0
+    for k in range(C + 1):
+        sum_of_node_k = 0
+        n_k = 0
+        degree_sum = 0
+        for i in range(label.shape[0]):
+            if label[i] == k:  # for each yv == k
+                n_k += 1
+                degree_sum += matrix[i].sum()
+                for j in range(matrix[i].shape[0]):
+                    if matrix[i][j]:
+                        if label[j] == k:
+                            sum_of_node_k += 1
+        temp = sum_of_node_k / degree_sum - n_k / matrix.shape[0]
+        c_h += max(0, temp)
+    c_h /= C
+    return c_h
+
+
+def D_k(matrix, label, k):
+    dk = 0
+    for i in range(label.shape[0]):
+        if label[i] == k:  # for each yv == k
+            dk += matrix[i].sum()
+    return dk
+
+
+def adjusted_homophily(matrix, label):
+    eh = edge_homophily(matrix, label)
+    e = label.shape[0]
+    c = label.max()
+    temp = 0
+    for k in range(c+1):
+        temp += pow(D_k(matrix, label, k) / (2 * e), 2)
+    ah = (eh - temp) / (1 - temp)
+    return ah
+
+def balanced_homophily(matrix, label):
+    c = label.max()
+    h_ball = 0
+    for k in range(c+1):
+        dk = D_k(matrix,label,k)
+        temp = 0
+        for i in range(label.shape[0]):
+            if label[i] == k :
+                for j in range(matrix[i].shape[0]):
+                    if j <= i:
+                        continue
+                    if (matrix[i][j] == 1) & (label[j] == k):
+                        temp += 1
+        h_ball += (temp / dk)
+    h_ball /= c
+    bh = (h_ball - 1/c) * c / (c-1)
+    return bh
+
+
+
+if __name__ == '__main__':
+    args = arg_parse()
+    matrix = get_matrix(args)
+    label = get_label(args)
+    if args.metric == 'edge_homophily':
+        metric_result = edge_homophily(matrix, label)
+    elif args.metric == 'node_homophily':
+        metric_result = node_homophily(matrix, label)
+    elif args.metric == 'class_homophily':
+        metric_result = class_homophily(matrix, label)
+    elif args.metric == 'adjusted_homophily':
+        metric_result = adjusted_homophily(matrix, label)
+    elif args.metric == 'balanced_homophily':
+        metric_result = balanced_homophily(matrix, label)
+
     print("dataset={}, attack={}, budget={}%,metric={}, result= {}".format(args.dataset, args.attack, args.budget,
-                                                                           args.metric, nh))
+                                                                           args.metric, metric_result))
