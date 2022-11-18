7e62a8c7d77f2870c2a551c45e49d9d450d434f2
kushr-zz <77181117+kushr-zz@users.noreply.github.com>
2022-11-18 15:26:35 +0800

    modified matrix load metric_generate.sh :generate result.csv result.csv in src/data

--- src/metric_generate.sh
+++ src/metric_generate.sh
@@ -4,15 +4,14 @@ dataSets="Cora CiteSeer"
 attacks="CLGA dice metattack minmax nodeembeddingattack pgd random "
 budgets="01 05 10 15 20"
 metrics="edge_homophily node_homophily class_homophily adjusted_homophily balanced_homophily"
-echo "exps","dataSets","attacks","budgets","metrics","result"> file.csv
+echo "exps","dataSets","attacks","budgets","metrics","result"> result.csv
 for exp in $exps; do
   for dataset in $dataSets; do
     for att in $attacks; do
       for budget in $budgets; do
         for metric in $metrics; do
           result=$(python metrics.py --exp $exp --dataset $dataset --attack $att --budget $budget --metric $metric)
-#         echo "---------------",$result
-          echo $exp,$dataset,$att,$budget,$metric,$result>> file.csv
+          echo $exp,$dataset,$att,$budget,$metric,$result>> result.csv
 
         done
       done
